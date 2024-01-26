e85caaba82b05782b41d5bb90c06edbe84b358ce
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2024-01-26 20:12:49 +0200

    express init

---
+++ solo-less-1/myapp/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Наше проміжне ПЗ");
+  res.send(req.body);
+  return;
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact page</h1>");
+});
+
+app.get("/contact/:id", (req, res) => {
+  res.send(`<h1>Contact</h1> Параметр: ${req.params.id}`);
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
